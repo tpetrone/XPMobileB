f0f20721107a65982c9513cfde6ac4b9121de4d2
JS.WilProf <penawjs@gmail.com>
2018-01-31 18:25:52 -0200

    Carga das Apresentações de Rest e do Template para Projeto em 31/01/2018 18:25

--- workspace/Mobile/src/br/senai/sp/informatica/mobileb/exemplos/Teste.java
+++ workspace/Mobile/src/br/senai/sp/informatica/mobileb/exemplos/Teste.java
@@ -1,14 +1,46 @@
 package br.senai.sp.informatica.mobileb.exemplos;
 
-import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Teste {
- public static void main(String[] args) {
-		String nome = JOptionPane.showInputDialog("Informe o seu Nome");
-		String temp = JOptionPane.showInputDialog("Informe o seu Salário");
-		double salario = Double.parseDouble(temp);
-		salario = salario * 1.05;
-		JOptionPane.showMessageDialog(null, nome + 
-			" seu novo salário é de R$ " + salario);
-  }
+	public static void main(String[] args) {
+//		String nome = JOptionPane.showInputDialog("Informe o seu Nome");
+//		String temp = JOptionPane.showInputDialog("Informe o seu Salário");
+//		double salario = Double.parseDouble(temp);
+//		salario = salario * 1.05;
+//		JOptionPane.showMessageDialog(null, nome + 
+//			" seu novo salário é de R$ " + salario);
+	 
+	 
+	 List<Xpto> lista = new ArrayList<Xpto>(Arrays.asList( new Xpto[] {
+		 new Xpto("fulano", "Rua A, 33"),
+		 new Xpto("beltrano", "Rua B, 44")
+	 }));
+	 
+	 lista.add(new Xpto("ciclano", "Rua C, 55"));
+	 
+	 lista.stream()
+	 	.map(t -> t.toString())
+	 	.forEach(System.out::println);
+
+	}
+}
+
+class Xpto {
+	private String nome;
+	private String endereco;
+
+	public Xpto(String nome, String endereco) {
+		super();
+		this.nome = nome;
+		this.endereco = endereco;
+	}
+
+	@Override
+	public String toString() {
+		return "nome: " + nome + " endereco: " + endereco;
+	}
+
 }
